52d3a6554cf46bed01d178f2d812ed0b4fcec96c
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-02 10:57:25 +0100

    Add excoveralls dep

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Astarte.VMQ.Plugin.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       deps: deps()
     ]
   end
@@ -22,8 +24,7 @@ defmodule Astarte.VMQ.Plugin.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.7", only: :test}
     ]
   end
 end
