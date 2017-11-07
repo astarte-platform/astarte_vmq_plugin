fdffa7937b532f2c5e6ad1fed8c5d49a6b61345a
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-07 16:42:39 +0100

    Add config_test

---
+++ test/astarte_vmq_plugin/config_test.exs
@@ -0,0 +1,29 @@
+defmodule Astarte.VMQ.Plugin.ConfigTest do
+  use ExUnit.Case
+
+  alias Astarte.VMQ.Plugin.Config
+
+  test "config init correctly converts amqp_options to elixir strings" do
+    opts =
+      [username: 'user',
+       password: 'password',
+       virtual_host: '/']
+
+    Application.put_env(:astarte_vmq_plugin, :amqp_options, opts)
+    Config.init()
+
+    new_opts = Config.amqp_options()
+    assert Keyword.get(new_opts, :username) == "user"
+    assert Keyword.get(new_opts, :password) == "password"
+    assert Keyword.get(new_opts, :virtual_host) == "/"
+  end
+
+  test "config init correctly converts queue_name to elixir string" do
+    queue_name = 'test_erlang_str'
+
+    Application.put_env(:astarte_vmq_plugin, :queue_name, queue_name)
+    Config.init()
+
+    assert Config.queue_name() == to_string(queue_name)
+  end
+end
