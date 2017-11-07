611e6844297d4a933b58f1bef393d28d0c6c5586
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-07 17:22:18 +0100

    config_test: put back original values after test
    Don't ruin the party for the other tests

--- test/astarte_vmq_plugin/config_test.exs
+++ test/astarte_vmq_plugin/config_test.exs
@@ -9,6 +9,8 @@ defmodule Astarte.VMQ.Plugin.ConfigTest do
        password: 'password',
        virtual_host: '/']
 
+    old_opts = Config.amqp_options()
+
     Application.put_env(:astarte_vmq_plugin, :amqp_options, opts)
     Config.init()
 
@@ -16,14 +18,24 @@ defmodule Astarte.VMQ.Plugin.ConfigTest do
     assert Keyword.get(new_opts, :username) == "user"
     assert Keyword.get(new_opts, :password) == "password"
     assert Keyword.get(new_opts, :virtual_host) == "/"
+
+    on_exit fn ->
+      Application.put_env(:astarte_vmq_plugin, :amqp_options, old_opts)
+    end
   end
 
   test "config init correctly converts queue_name to elixir string" do
     queue_name = 'test_erlang_str'
 
+    old_queue_name = Config.queue_name()
+
     Application.put_env(:astarte_vmq_plugin, :queue_name, queue_name)
     Config.init()
 
     assert Config.queue_name() == to_string(queue_name)
+
+    on_exit fn ->
+      Application.put_env(:astarte_vmq_plugin, :amqp_options, old_queue_name)
+    end
   end
 end
