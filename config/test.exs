8317d6f0b0ec1b74b01a0c98601db28dc5577591
Riccardo Binetti <riccardo.binetti@ispirata.com>
2019-11-19 17:14:09 +0100

    config/test: allow configuring rabbitmq host with env var
    Signed-off-by: Riccardo Binetti <riccardo.binetti@ispirata.com>

--- config/test.exs
+++ config/test.exs
@@ -18,8 +18,7 @@
 
 use Mix.Config
 
-config :astarte_vmq_plugin, :amqp_options,
-  host: "rabbitmq"
+config :astarte_vmq_plugin, :amqp_options, host: System.get_env("RABBITMQ_HOST") || "rabbitmq"
 
 config :astarte_vmq_plugin, :queue_name,
   "test_queue"
