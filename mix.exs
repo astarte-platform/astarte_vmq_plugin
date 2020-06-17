ba7699f3c5da442d2c8758424f61c7171c413046
Davide Bettio <davide.bettio@ispirata.com>
2020-06-17 13:16:37 +0200

    mix.exs: update minimum Elixir version to 1.10

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Astarte.VMQ.Plugin.Mixfile do
     [
       app: :astarte_vmq_plugin,
       version: "1.0.0-dev",
-      elixir: "~> 1.8",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
