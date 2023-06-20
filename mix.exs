ab761ebc3a79b5513cf125c2c2bb584ee5770975
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-06-20 16:07:56 +0200

    Update astarte_rpc dependecy
    Contextually, bump protobuf to 0.12 (hex.pm) version
    
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule Astarte.VMQ.Plugin.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_rpc, github: "astarte-platform/astarte_rpc", branch: "release-1.1"}
+      {:astarte_rpc, "~> 1.1"}
     ]
   end
 
